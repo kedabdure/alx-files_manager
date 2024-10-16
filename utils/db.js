b83438e2f1c1f050d0d6341578f2ca089fdc834d
kedabudre <abdurehimk77@gmail.com>
2024-10-16 09:52:32 +0300

    update mongodb setup

--- utils/db.js
+++ utils/db.js
@@ -1,39 +1,36 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient } from 'mongodb';
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}`;
 
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'file_manager';
-
-    this.url = `mongodb://${host}:${port}`;
-    this.databaseName = database;
-    this.client = new MongoClient(this.url, {useNewUrlParser: true, userUnifiedTopology: true})
-
-    this.client.connect()
-    .then(() => {
-      console.log(`Connected to mongodb ${this.url}`);
-    })
-    .catch((error) => {
-      console.error(`MongoDB connection error: ${error.message}`)
-    })
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        this.db = client.db(DB_DATABASE);
+        this.usersCollection = this.db.collection('users');
+        this.filesCollection = this.db.collection('files');
+      } else {
+        console.log(err.message);
+        this.db = false;
+      }
+    });
   }
 
   isAlive() {
-    return this.client.isConnected();
+    return Boolean(this.db);
   }
 
   async nbUsers() {
-    const db = this.client.db(this.databaseName);
-    const count = await usersCollection.countDocuments();
-    return count;
+    const numberOfUsers = this.usersCollection.countDocuments();
+    return numberOfUsers;
   }
 
   async nbFiles() {
-    const db = this.client.db(this.databaseName);
-    const filesCollection = db.collection('files');
-    const count = await filesCollection.countDocuments();
-    return count;
+    const numberOfFiles = this.filesCollection.countDocuments();
+    return numberOfFiles;
   }
 }
 
