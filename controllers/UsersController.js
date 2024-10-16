b4478eca2419ef28c24c532d17231bb461969e92
kedabudre <abdurehimk77@gmail.com>
2024-10-16 18:41:00 +0300

    update the userController

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,9 +1,13 @@
+import { ObjectId } from 'mongodb';
 import sha1 from 'sha1';
+import Queue from 'bull';
 import dbClient from '../utils/db';
+import userUtils from '../utils/user';
 
 const userQueue = new Queue('userQueue');
 
 class UsersController {
+
   static async postNew(request, response) {
     const { email, password } = request.body;
 
@@ -38,6 +42,22 @@ class UsersController {
     });
 
     return response.status(201).send(user);
+  }
+
+  static async getMe(request, response) {
+    const { userId } = await userUtils.getUserIdAndKey(request);
+
+    const user = await userUtils.getUser({
+      _id: ObjectId(userId),
+    });
+
+    if (!user) return response.status(401).send({ error: 'Unauthorized' });
+
+    const processedUser = { id: user._id, ...user };
+    delete processedUser._id;
+    delete processedUser.password;
+
+    return response.status(200).send(processedUser);
   }
 }
 
